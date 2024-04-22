7a9a28a5fae71cc0816344600b535c2f4c601dd9
Ann Mary John <amjohn@andrew.cmu.edu>
2024-04-22 01:05:17 +0000

    initialization file added.

---
+++ ScholarQuest/__init__.py
@@ -0,0 +1,10 @@
+"""Top level initialization functions."""
+from .utils import get_publications_by_topic
+from .utils import get_author_h_index
+from .utils import get_institution_details
+from .utils import calculate_institution_scores
+from .utils import calculate_author_scores
+from .utils import get_top_professors
+from .utils import get_top_institutions_and_professors
+
+from .main import main
