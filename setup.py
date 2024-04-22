422a38e54bdb360e3bf679b741eb3d5500268563
Ann Mary John <amjohn@andrew.cmu.edu>
2024-04-22 02:33:10 +0000

    setup file changed for flake8.

--- setup.py
+++ setup.py
@@ -2,26 +2,21 @@
 from setuptools import setup
 
 setup(
-    name='ScholarQuest',
-    version='0.0.1',
+    name="ScholarQuest",
+    version="0.0.1",
     description="""Package to get top institutions and\
     top professors/authors in the world or a specified country.""",
-    maintainer='Ann John',
-    maintainer_email='amjohn@andrew.cmu.edu',
-    license='MIT',
-    packages=['ScholarQuest'],
-    install_requires=['requests'],
+    maintainer="Ann John",
+    maintainer_email="amjohn@andrew.cmu.edu",
+    license="MIT",
+    packages=["ScholarQuest"],
+    install_requires=["requests"],
     scripts=[],
-    entry_points={
-        'console_scripts':
-        [
-            'sq = ScholarQuest.main:main'
-        ]
-    },
+    entry_points={"console_scripts": ["sq = ScholarQuest.main:main"]},
     long_description="""\
         This package is pip installable and allows users to input a\
         topic of their interest and a country (optional - if not\
         provided, it will default to the world) to get the top 10\
         institutions and top professors/authors.
-    """
+    """,
 )
